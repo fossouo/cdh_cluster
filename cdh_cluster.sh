d38b35c0e05c8518c2f9b71e048ec5d2b571b73f
root <fossouodonald@gmail.com>
2017-05-24 14:38:41 +0000

    Rollback regression Yarn

--- cdh_cluster.sh
+++ cdh_cluster.sh
@@ -18,16 +18,16 @@ docker network create cloudera-network
 
 #3. Start HDFS
 
-docker run -d --net cloudera-network --net-alias cdh-namenode -p 8020:8020 dfossouo/cdh-namenode:5.9.0
+docker run -d --net cloudera-network --net-alias cdh-namenode -p 8020:8020 dfossouo/cloudera-namenode:5.9.0
 
 #4. Start YARN
 
-docker run -d --net cloudera-network --net-alias cdh-yarnmaster -p 8088:8088 -p 8032:8032 dfossouo/cdh-yarnmaster:5.9.0
+docker run -d --net cloudera-network --net-alias cdh-yarn -p 8088:8088 -p 8032:8032 dfossouo/cloudera-yarn:5.9.0
 
 #5. Start DataNode
 
-docker run -d --net cloudera-network --net-alias cloudera-datanode -h cloudera-datanode --link cdh-namenode --link cdh-yarnmaster -p 50020:50020 -p 50075:50075 -p 8042:8042 dfossouo/cloudera-datanode:5.9.0 
+docker run -d --net cloudera-network --net-alias cloudera-datanode -h datanode --link cdh-namenode --link cdh-yarn -p 50020:50020 -p 50075:50075 -p 8042:8042 dfossouo/cloudera-datanode:5.9.0 
 
 #6. Launch Edge Node
 
-docker run -ti --net cloudera-network --net-alias cloudera-edge --link cdh-namenode --link cdh-yarnmaster dfossouo/cloudera-edge:5.9.0 bash
+docker run -ti --net cloudera-network --net-alias cloudera-edge --link cdh-namenode --link cdh-yarn dfossouo/cloudera-edge:5.9.0 bash
